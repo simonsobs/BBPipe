a9376b319086fb7908ef717f794a5c5389be6556
Josquin Errard <josquin.errard@gmail.com>
2019-01-29 16:02:11 +0100

    initial commit

---
+++ test_mapbased_param/__init__.py
@@ -0,0 +1,7 @@
+from bbpipe import PipelineStage
+from .mask_preproc import BBMaskPreproc
+from .maps_preproc import BBMapsPreproc
+from .null_tester import BBNullTester
+from .power_specter import BBPowerSpecter
+from .covfefe import BBCovFeFe
+from .compsep import BBCompSep
