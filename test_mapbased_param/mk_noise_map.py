30e8ce6d856c4c3a22d819864eac915f8cc83f10
Josquin Errard <josquin.errard@gmail.com>
2019-02-07 16:04:35 +0100

    quick fgs and inh noise and fg simulator

---
+++ test_mapbased_param/mk_noise_map.py
@@ -0,0 +1,87 @@
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+import V3calc as v3
+import os
+
+def get_nhits(nside_out=64) :
+    """
+    Generates an Nhits map in Galactic coordinates.
+    nside_out : output resolution
+    """
+    fname_out='norm_nHits_SA_35FOV_G.fits'
+
+    if not os.path.isfile(fname_out) :
+        fname_in='norm_nHits_SA_35FOV.fits'
+        mp_C=hp.read_map(fname_in,verbose=False)
+        nside_l=hp.npix2nside(len(mp_C))
+
+        nside_h=512
+        ipixG=np.arange(hp.nside2npix(nside_h))
+        thG,phiG=hp.pix2ang(nside_h,ipixG)
+        r=hp.Rotator(coord=['G','C'])
+        thC,phiC=r(thG,phiG)
+        ipixC=hp.ang2pix(nside_l,thC,phiC)
+
+        mp_G=hp.ud_grade(mp_C[ipixC],nside_out=nside_l)
+        hp.write_map(fname_out,mp_G)
+
+    return hp.ud_grade(hp.read_map(fname_out,verbose=False),
+                       nside_out=nside_out)
+
+def get_mask(nside_out=512) :
+    """
+    Generates inverse-variance mask from Nhits map
+    nside_out : output resolution
+    """
+    zer0=1E-6
+    nh=get_nhits(nside_out=nside_out)
+    nh/=np.amax(nh)
+    msk=np.zeros(len(nh))
+    not0=np.where(nh>zer0)[0]
+    msk[not0]=nh[not0]
+    return msk
+
+def get_noise_sim(sensitivity=2,knee_mode=1,ny_lf=1.,nside_out=512) :
+    """
+    Generates noise simulation
+    sensitivity : choice of sensitivity model for SAC's V3 
+    knee_mode : choice of ell_knee model for SAC's V3
+    ny_lf : number of years with an LF tube
+    nside_out : output resolution
+    """
+    nh=get_nhits(nside_out=nside_out)
+    msk=get_mask(nside_out=nside_out)
+    fsky=np.mean(msk)
+    ll,nll,nlev=v3.so_V3_SA_noise(sensitivity,knee_mode,ny_lf,fsky,3*nside_out,remove_kluge=True)
+    zer0=1E-6
+    id_cut=np.where(nh<zer0)[0]
+    nh[id_cut]=np.amax(nh) #zer0
+    mps_no=[];
+    for i_n in np.arange(len(nll)) :
+        n=nll[i_n]
+        nl=np.zeros(3*nside_out)
+        nl[2:]=n; nl[:2]=n[0]
+        no_t,no_q,no_u=hp.synfast([nl/2.,nl,nl,0*nl,0*nl,0*nl],nside=nside_out,
+                                  pol=True,new=True,verbose=False)
+        nv_t=nlev[i_n]*np.ones_like(no_t)/np.sqrt(2.);
+        nv_q=nlev[i_n]*np.ones_like(no_q); nv_u=nlev[i_n]*np.ones_like(no_u)
+        no_t/=np.sqrt(nh/np.amax(nh)); no_q/=np.sqrt(nh/np.amax(nh)); no_u/=np.sqrt(nh/np.amax(nh));
+        nv_t/=np.sqrt(nh/np.amax(nh)); nv_q/=np.sqrt(nh/np.amax(nh)); nv_u/=np.sqrt(nh/np.amax(nh));
+        # mps_no.append([no_t,no_q,no_u])
+        mps_no.append(no_t)
+        mps_no.append(no_q)
+        mps_no.append(no_u)
+
+    mps_no=np.array(mps_no)
+    return msk,mps_no,nlev
+
+# nside_run=512
+# mask,maps,nlev=get_noise_sim()
+
+# mp_plot=maps[3][1]*mask
+# mp_plot[mask<=0]=hp.UNSEEN
+
+# hp.mollview(mp_plot,title='Noise (* inv. var. mask) @ 150 GHz, Q',coord=['G','C'])
+# plt.savefig('noise.png',bbox_inches='tight')
+# plt.show()
---
+++ test_mapbased_param/noise_fgs_generator.py
@@ -0,0 +1,112 @@
+"""
+Imhomogeneous noise
++ CMB + foregrounds 
+SAT frequency maps
+generator 
+"""
+
+import healpy as hp
+import numpy as np
+import pylab as pl
+import argparse
+import pysm
+from pysm.nominal import models
+import mk_noise_map as mknm
+import V3calc as V3
+
+def grabargs():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--sensitivity_mode", type=int,
+						help = "sensitivity mode for SATs",
+						default=1)
+	parser.add_argument("--knee_mode", type=int,
+						help = "knee mode for SATs",
+						default=1)
+	parser.add_argument("--low_freq_year", type=float,
+						help = "Number of years for the low frequency channels",
+						default=1.0)	
+	parser.add_argument("--nside", type=int,
+						help = "nside for the output maps resolutions",
+						default=128)	
+	parser.add_argument("--output_director", type=str, \
+						help = "folder to output fits files", \
+						default='.')
+	parser.add_argument("--tag", type=str, \
+						help = "specific tag for this run", \
+						default='SO_sims')
+	args = parser.parse_args()
+	return args
+
+
+def main():
+
+	args = grabargs()
+	# GENERATE NOISE MAP
+	nhits,noise_maps,nlev = mknm.get_noise_sim(sensitivity=args.sensitivity_mode, 
+					knee_mode=args.knee_mode,ny_lf=args.low_freq_year,nside_out=args.nside)
+	binary_mask = nhits*0.0
+	binary_mask[np.where(nhits!=0.0)[0]]=1.0
+	# GENERATE CMB AND FOREGROUNDS
+	d_config = models("d1", args.nside)
+	s_config = models("s1", args.nside)
+	c_config = models("c1", args.nside)
+	sky_config = {'cmb' : c_config, 'dust' : d_config, 'synchrotron' : s_config}
+	sky = pysm.Sky(sky_config)
+	# DEFINE INSTRUMENT AND SCAN SKY
+	fwhm = V3.so_V3_SA_beams()
+	freqs = V3.so_V3_SA_bands()
+	instrument_config = {
+	    'nside' : args.nside,
+	    'frequencies' : freqs, 
+	    'use_smoothing' : False,
+	    'beams' : fwhm, 
+	    'add_noise' : False,
+	    'sens_I' : nlev/np.sqrt(2),
+	    'sens_P' : nlev,
+	    'noise_seed' : 1234,
+	    'use_bandpass' : False,
+	    'output_units' : 'uK_CMB',
+	    'output_directory' : './',
+	    'output_prefix' : args.tag,
+		}
+
+	instrument = pysm.Instrument(instrument_config)
+	
+	# instrument.observe(sky)
+	freq_maps = instrument.observe(sky, write_outputs=False)[0]
+	# restructuration of the noise map
+	freq_maps = freq_maps.reshape(noise_maps.shape)
+	# adding noise
+	freq_maps += noise_maps
+	freq_maps += noise_maps
+	freq_maps *= binary_mask
+	noise_maps *= binary_mask
+	freq_maps[:,np.where(binary_mask==0)[0]] = hp.UNSEEN
+	noise_maps[:,np.where(binary_mask==0)[0]] = hp.UNSEEN
+
+	# noise covariance 
+	noise_cov = freq_maps*0.0
+	noise_cov[::3,:] = nlev[:,np.newaxis]/np.sqrt(2.0)
+	noise_cov[1::3,:] = nlev[:,np.newaxis]
+	noise_cov[2::3,:] = nlev[:,np.newaxis]
+	noise_cov *= binary_mask
+	noise_cov /= np.sqrt(nhits/np.amax(nhits))
+	noise_cov[:,np.where(binary_mask==0)[0]] = hp.UNSEEN
+
+	# save on disk frequency maps, noise maps, noise_cov, binary_mask
+	tag = '_nside'+str(args.nside)
+	tag += '_sens'+str(args.sensitivity_mode)
+	tag += '_knee'+str(args.knee_mode)
+	tag += '_nylf'+str(args.low_freq_year)
+
+	column_names = []
+	[ column_names.extend( ('I_'+str(ch)+'GHz','Q_'+str(ch)+'GHz','U_'+str(ch)+'GHz')) for ch in freqs]
+	hp.write_map(instrument_config['output_prefix']+'_binary_mask.fits', binary_mask, overwrite=True)
+	hp.write_map(instrument_config['output_prefix']+'_noise_mask.fits', nhits, overwrite=True)
+	hp.write_map(instrument_config['output_prefix']+'_frequency_maps'+tag+'.fits', freq_maps, overwrite=True, column_names=column_names)
+	hp.write_map(instrument_config['output_prefix']+'_noise_cov'+tag+'.fits', noise_cov, overwrite=True, column_names=column_names)
+	hp.write_map(instrument_config['output_prefix']+'_noise_maps'+tag+'.fits', noise_maps, overwrite=True, column_names=column_names)
+
+if __name__ == "__main__":
+
+	main( )
