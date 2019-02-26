fd12591b872f86749a95565e3ce8e53d328d6be7
Josquin Errard <josquin.errard@gmail.com>
2019-02-26 16:08:21 +0100

    modifying Cl and adding r estimators

--- test_mapbased_param/Cl_estimation.py
+++ test_mapbased_param/Cl_estimation.py
@@ -6,15 +6,14 @@ import pymaster as nmt
 
 class BBClEstimation(PipelineStage):
     """
-    Stage that performs three things:
-        * fit of the spectral indices, using fgbuster tools, along with their error bars
-        * estimate sky components, foregrounds and CMB
-        * estimate components' covariance
+    Stage that performs estimate the angular power spectra of:
+        * each of the sky components, foregrounds and CMB, as well as the cross terms
+        * of the corresponding covariance
     """
 
     name='BBClEstimation'
     inputs=[('binary_mask',FitsFile),('post_compsep_maps',FitsFile), ('post_compsep_cov',FitsFile)]
-    outputs=[('Cl_clean', FitsFile),('Cl_cov_clean', FitsFile)]
+    inputs=[('Cl_clean', FitsFile),('Cl_cov_clean', FitsFile)]
 
     clean_map = hp.read_map(self.get_input('post_compsep_maps'),verbose=False, field=None, h=True)
     cov_map = hp.read_map(self.get_input('post_compsep_cov'),verbose=False, field=None, h=True)
@@ -42,7 +41,8 @@ class BBClEstimation(PipelineStage):
     ncomp = int(len(clean_map)/2)
     Cl_clean = [] 
     Cl_cov_clean = []
-    for comp_i in range(ncomp):
+    components = []
+    for (comp_i) in range(ncomp):
         for comp_j in range(ncomp)[comp_i:]:
 
             print('building f ... ')
@@ -56,9 +56,11 @@ class BBClEstimation(PipelineStage):
             f_cov_map = nmt.NmtField(mask,[mask*cov_map,mask*cov_map],purify_b=False)
 
             print('computing Cl_NaMaster ... ')
+            components.append(str((comp_i,comp_j))) 
             Cl_clean.append(compute_master(f_cov_map,f_cov_map))
             Cl_cov_clean.append(compute_master(f_cov_map,f_cov_map))
 
     print('saving to disk ... ')
+    print(components)
     hp.fitsfunc.write_cl(self.get_output('Cl_clean'), Cl_clean, overwrite=False)
     hp.fitsfunc.write_cl(self.get_output('Cl_cov_clean'), Cl_cov_clean, overwrite=False)
