c3b582d02c700046ae30f990828169338b2ffb47
Josquin Errard <josquin.errard@gmail.com>
2019-03-12 14:31:53 +0100

    updating Cl estimator

--- test_mapbased_param/Cl_estimation.py
+++ test_mapbased_param/Cl_estimation.py
@@ -4,6 +4,7 @@ import numpy as np
 import pylab as pl
 import pymaster as nmt
 import healpy as hp
+from fgbuster.cosmology import _get_Cl_cmb 
 
 class BBClEstimation(PipelineStage):
     """
@@ -33,14 +34,26 @@ class BBClEstimation(PipelineStage):
         print('building ell_eff ... ')
         ell_eff = b.get_effective_ells()
         
-        def compute_master(fl1,fl2) :
-            cl=w.decouple_cell(nmt.compute_coupled_cell(fl1,fl2))
-            cell=[]
-            cell.append(ell_eff)
-            for c in cl :
-                cell.append(c)
-            cell=np.array(cell)
-            return cell
+        #Read power spectrum and provide function to generate simulated skies
+        cltt,clee,clbb,clte = hp.read_cl(self.config['Cls_fiducial'])[:,:4000]
+        mp_t_sim,mp_q_sim,mp_u_sim=hp.synfast([cltt,clee,clbb,clte], nside=nside_map, new=True, verbose=False)
+
+        def get_field(mp_q,mp_u) :
+            #This creates a spin-2 field with both pure E and B.
+            f2_y=nmt.NmtField(mask_apo,[mp_q,mp_u],purify_e=False,purify_b=True)
+            return f2y0
+
+        #We initialize two workspaces for the non-pure and pure fields:
+        f2y0=get_field(mp_q_sim,mp_u_sim)
+        w.compute_coupling_matrix(f2y0,f2y0,b)
+
+        #This wraps up the two steps needed to compute the power spectrum
+        #once the workspace has been initialized
+        def compute_master(f_a,f_b,wsp) :
+            cl_coupled=nmt.compute_coupled_cell(f_a,f_b)
+            cl_decoupled=wsp.decouple_cell(cl_coupled)
+            return cl_decoupled
+
 
         ncomp = int(len(clean_map)/2)
         Cl_clean = [ell_eff] 
@@ -55,12 +68,7 @@ class BBClEstimation(PipelineStage):
                 print('clean_map.shape = ', clean_map.shape)
                 print('cov_map.shape = ', cov_map.shape)
 
-                print('building f ... ')
-                f=nmt.NmtField(mask_apo,[mask*clean_map[2*comp_i],mask*clean_map[2*comp_i+1]], purify_b=self.config['purify_b'])
-
-                print('building w ... ')
-                w.compute_coupling_matrix(f,f,b)
-
+                """
                 print('building f ... ')
                 f_clean_map_i = nmt.NmtField(mask,[mask*clean_map[2*comp_i],mask*clean_map[2*comp_i+1]], purify_b=self.config['purify_b'])
                 f_clean_map_j = nmt.NmtField(mask,[mask*clean_map[2*comp_j],mask*clean_map[2*comp_j+1]], purify_b=self.config['purify_b'])
@@ -71,6 +79,16 @@ class BBClEstimation(PipelineStage):
                 components.append(str((comp_i,comp_j))) 
                 Cl_clean.append(compute_master(f_clean_map_i,f_clean_map_j)[3] )
                 Cl_cov_clean.append(compute_master(f_cov_map_i,f_cov_map_j)[3] )
+                """
+
+                fyp_i=get_field(clean_map[2*comp_i], clean_map[2*comp_i+1])
+                fyp_j=get_field(clean_map[2*comp_j], clean_map[2*comp_j+1])
+
+                fyp_cov_i=get_field(cov_map[2*comp_i,2*comp_i], cov_map[2*comp_i+1,2*comp_i+1])
+                fyp_cov_j=get_field(cov_map[2*comp_j,2*comp_j], cov_map[2*comp_j+1,2*comp_j+1])
+
+                Cl_clean.append(compute_master(fyp_i,fyp_j)[3] )
+                Cl_cov_clean.append(compute_master(fyp_cov_i,fyp_cov_j)[3] )
 
         print('saving to disk ... ')
         hp.fitsfunc.write_cl(self.get_output('Cl_clean'), np.array(Cl_clean), overwrite=True)
