f29e4aef0629f7358460ed085ea592480297d370
Josquin Errard <josquin.errard@gmail.com>
2019-03-12 14:59:07 +0100

    testing maps

--- test_mapbased_param/Cl_estimation.py
+++ test_mapbased_param/Cl_estimation.py
@@ -38,6 +38,11 @@ class BBClEstimation(PipelineStage):
         cltt,clee,clbb,clte = hp.read_cl(self.config['Cls_fiducial'])[:,:4000]
         mp_t_sim,mp_q_sim,mp_u_sim=hp.synfast([cltt,clee,clbb,clte], nside=nside_map, new=True, verbose=False)
 
+        hp.mollview(mp_q_sim)
+        pl.figure()
+        hp.mollview(clean_map[0])
+        pl.show()
+
         def get_field(mp_q,mp_u) :
             #This creates a spin-2 field with both pure E and B.
             f2y=nmt.NmtField(mask_apo,[mp_q,mp_u],purify_e=False,purify_b=True)
@@ -64,8 +69,8 @@ class BBClEstimation(PipelineStage):
 
                 print('comp_i = ', comp_i)
                 print('comp_j = ', comp_j)
-                print('clean_map.shape = ', clean_map.shape)
-                print('cov_map.shape = ', cov_map.shape)
+                # print('clean_map.shape = ', clean_map.shape)
+                # print('cov_map.shape = ', cov_map.shape)
 
                 """
                 print('building f ... ')
