97af0035c2eb9972219677b86cd6d73589382abe
Josquin Errard <josquin.errard@gmail.com>
2019-03-14 10:57:25 +0100

    debugging estimation of r

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -76,6 +76,7 @@ class BBREstimation(PipelineStage):
         Cl_BB_lens = _get_Cl_cmb(1.,0.)[2]#[lmin:lmax]
         Cl_BB_prim = _get_Cl_cmb(0.0,self.config['r_input'])[2]#[lmin:lmax]
         bins = nmt.NmtBin(self.config['nside'], nlb=int(1./self.config['fsky']))
+        print np.shape(Cl_BB_lens)
         Cl_BB_lens_bin = bins.bin_cell(Cl_BB_lens)
 
         ClBB_model_other_than_prim =  Cl_BB_lens_bin + Cl_cov_clean[1][(ell_v>=lmin)&(ell_v<=lmax)]
