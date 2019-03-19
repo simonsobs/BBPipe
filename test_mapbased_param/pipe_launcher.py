5fdbcc014d3d3cb0a8d36552c4f44a7deb1459cd
Josquin Errard <josquin.errard@gmail.com>
2019-03-19 14:58:18 +0100

    debug

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -165,7 +165,7 @@ def main():
                 ny_lf=args.ny_lf, noise_option=args.noise_option, dust_marginalization=args.dust_marginalization,\
                 path_to_temp_files=args.path_to_temp_files)
 
-        subprocess.call(["bbpipe", os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml")])
+        subprocess.call(["/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe", os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml")])
 
     ####################
     barrier()
