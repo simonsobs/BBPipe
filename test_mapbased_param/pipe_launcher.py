79af6bc8ee592d05f0ad10ec8679ebfaadb9d7ba
Josquin Errard <josquin.errard@gmail.com>
2019-03-20 16:38:30 +0100

    test check_output()

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -197,7 +197,8 @@ def main():
                 path_to_temp_files=args.path_to_temp_files, r_input=args.r_input)
         # submit call 
         print("subprocess call = ", "/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe", os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"))
-        p = subprocess.call("/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe "+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"), shell=True)
+        # p = subprocess.call("/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe "+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"), shell=True, stdout=subprocess.PIPE)
+        p = subprocess.check_output("/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe "+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"), shell=True, stdout=subprocess.PIPE)
 
     ####################
     barrier()
