435c358c18f7463239afc1d812bfffb20e189ead
Josquin Errard <josquin.errard@gmail.com>
2019-03-29 16:53:08 +0100

    debug parametric cleaning

--- test_mapbased_param/mapbased_parametric_analysis.py
+++ test_mapbased_param/mapbased_parametric_analysis.py
@@ -105,7 +105,7 @@ class BBMapParamCompSep(PipelineStage):
         for p in obs_pix:
             inv_AtNA = np.linalg.inv(A_maxL_loc.T.dot(1.0/noise_cov_diag[:,:,p]).dot(A_maxL_loc))
             print('shape(inv_AtNA) = ',np.shape(inv_AtNA))
-            print('ATNd = ',  A_maxL_loc.T.dot(1.0/noise_cov_diag[:,:,p]).dot(noise_maps__[:,p]))
+            print('ATNd = ',  np.shape(A_maxL_loc.T.dot(1.0/noise_cov_diag[:,:,p]).dot(noise_maps__[:,p])))
             noise_after_comp_sep[:,p] = inv_AtNA.dot( A_maxL_loc.T ).dot(1.0/noise_cov_diag[:,:,p]).dot(noise_maps__[:,p])
 
         hp.write_map(self.get_output('post_compsep_noise'), noise_after_comp_sep, overwrite=True)
