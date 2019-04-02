a024961f060fd1a6452eef8feb30aac2e3fc4fb1
Josquin Errard <josquin.errard@gmail.com>
2019-04-02 11:55:02 +0200

    change output figure

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -58,7 +58,7 @@ class BBREstimation(PipelineStage):
                     pl.loglog( ell_v_loc, norm*Cov_model, label='modeled BB', color='k', linestyle='-', linewidth=2.0, alpha=0.8)
                     pl.legend()
                     pl.xlabel('$\ell$', fontsize=20)
-                    pl.xlabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
+                    pl.ylabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
                     pl.savefig(self.get_output('power_spectrum_post_comp_sep'))
                     pl.close()
 
@@ -165,7 +165,7 @@ class BBREstimation(PipelineStage):
                     pl.loglog( ell_v_loc, norm*Cov_model, label='modeled BB', color='k', linestyle='-', linewidth=2.0, alpha=0.8)
                     pl.legend()
                     pl.xlabel('$\ell$', fontsize=20)
-                    pl.xlabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
+                    pl.ylabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
                     pl.savefig(self.get_output('power_spectrum_post_comp_sep'))
                     pl.close()
 
