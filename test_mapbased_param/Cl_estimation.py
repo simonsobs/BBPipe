b12353cb1360a1bdd38911c3f735cfee7394f786
Josquin Errard <josquin.errard@gmail.com>
2019-04-04 17:42:02 +0200

    debugging noise post comp sep

--- test_mapbased_param/Cl_estimation.py
+++ test_mapbased_param/Cl_estimation.py
@@ -97,7 +97,7 @@ class BBClEstimation(PipelineStage):
         ### compute noise bias in the comp sep maps
         Cl_cov_clean_loc = []
         for f in range(len(self.config['frequencies'])):
-            fn = get_field(mask*noise_maps[3*f+1,:]*KCMB2RJ(self.config['frequencies'][f]), mask*noise_maps[3*f+2,:]*KCMB2RJ(self.config['frequencies'][f]))
+            fn = get_field(mask*noise_maps[3*f+1,:]*KCMB2RJ(150.0), mask*noise_maps[3*f+2,:]*KCMB2RJ(150.0))
             # hp.mollview(noise_maps[3*f+1,:], title='Q', sub=121)
             # hp.mollview(noise_maps[3*f+2,:], title='U', sub=122)
             Cl_cov_clean_loc.append(1.0/compute_master(fn, fn, w)[3] )
