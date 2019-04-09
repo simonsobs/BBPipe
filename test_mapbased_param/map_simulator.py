b41b3f97fe92e911b9923c1dc6c3cb454db9b05d
Josquin Errard <josquin.errard@gmail.com>
2019-04-09 16:57:57 +0200

    understanding difference between white and 1/f noise simulations

--- test_mapbased_param/map_simulator.py
+++ test_mapbased_param/map_simulator.py
@@ -81,12 +81,7 @@ class BBMapSim(PipelineStage):
                 nlev_map[3*i:3*i+3,:] = np.array([instrument_config['sens_I'][i], instrument_config['sens_P'][i], instrument_config['sens_P'][i]])[:,np.newaxis]*np.ones((3,freq_maps.shape[-1]))
             # nlev_map = np.vstack(([instrument_config['sens_I'], instrument_config['sens_P'], instrument_config['sens_P']]))
             nlev_map /= hp.nside2resol(self.config['nside'], arcmin=True)
-            [hp.mollview(noise_maps[i]*binary_mask, norm='hist', title=str(i)) for i in range(len(noise_maps))]
             noise_maps = np.random.normal(freq_maps*0.0, nlev_map, freq_maps.shape)*binary_mask
-            # hp.mollview(noise_maps[0]*binary_mask, norm='hist')
-            [hp.mollview(noise_maps[i]*binary_mask, norm='hist', title=str(i)) for i in range(len(noise_maps))]
-            pl.show()
-            exit()
             freq_maps += noise_maps*binary_mask
         elif self.config['noise_option']=='no_noise': 
             pass
