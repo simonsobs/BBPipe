2a939bdd8572e1c774b86572bee67ef2072ead3f
Josquin Errard <josquin.errard@gmail.com>
2019-04-15 15:25:54 +0200

    reorganizing

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -35,67 +35,6 @@ class BBREstimation(PipelineStage):
 
         ell_v = Cl_clean[0]        
         
-        def from_Cl_to_r_estimate(ClBB_obs, ell_v, fsky, Cl_BB_prim, ClBB_model_other_than_prim, r_v, bins, Cl_BB_lens_bin, **minimize_kwargs):
-
-            def likelihood_on_r_computation( r_loc, make_figure=False ):
-                '''
-                -2logL = sum_ell [ (2l+1)fsky * ( log(C) + C^-1.D  ) ]
-                    cf. eg. Tegmark 1998
-                '''    
-                Cov_model = bins.bin_cell(Cl_BB_prim[:3*self.config['nside']]*r_loc)[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]\
-                                            + ClBB_model_other_than_prim
-                if make_figure:
-                    pl.figure( figsize=(10,7), facecolor='w', edgecolor='k' )
-                    ell_v_loc = ell_v[(ell_v>=lmin)&(ell_v<=lmax)]
-                    norm = ell_v_loc*(ell_v_loc+1)/2/np.pi
-                    pl.loglog( ell_v_loc, norm*bins.bin_cell(Cl_BB_prim[:3*self.config['nside']]*r_loc)[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])],
-                                 label='primordial BB, r = '+str(r_loc), linestyle='--', color='Purple', linewidth=2.0 )
-                    pl.loglog( ell_v_loc, norm*Cl_BB_lens_bin[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])], 
-                                label='lensing BB', linestyle='-', color='DarkOrange', linewidth=2.0)
-                    pl.loglog( ell_v_loc, norm*Cl_cov_clean[1][(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])], 
-                                label='noise post comp sep', linestyle=':', color='DarkBlue')
-                    pl.loglog( ell_v_loc, norm*ClBB_obs, label='observed BB', color='red', linestyle='-', linewidth=2.0, alpha=0.8)
-                    pl.loglog( ell_v_loc, norm*Cov_model, label='modeled BB', color='k', linestyle='-', linewidth=2.0, alpha=0.8)
-                    pl.legend()
-                    pl.xlabel('$\ell$', fontsize=20)
-                    pl.ylabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
-                    pl.savefig(self.get_output('power_spectrum_post_comp_sep'))
-                    pl.close()
-
-                # logL = np.sum( (2*ell_v[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]+1)*fsky\
-                                    # *( np.log( Cov_model ) + ClBB_obs/Cov_model ))
-
-                logL = 0.0 
-                for b in range(len(ClBB_obs)):
-                    logL += np.sum((2* bins.get_ell_list(b)+1))*fsky*( np.log( Cov_model[b] ) + ClBB_obs[b]/Cov_model[b] )
-                return logL
-
-            # gridding -2log(L)
-            logL = r_v*0.0
-            for ir in range(len(r_v)):
-                logL[ir] = likelihood_on_r_computation( r_v[ir] )
-                ind = ir*100.0/len(r_v)
-                sys.stdout.write("\r  .......... gridding the likelihood on tensor-to-scalar ratio >>>  %d %% " % ind )
-                sys.stdout.flush()
-            sys.stdout.write("\n")
-            # renormalizing logL 
-            chi2 = (logL - np.min(logL))
-            # computing the likelihood itself, for plotting purposes
-            likelihood_on_r = np.exp( - chi2 )/np.max(np.exp( - chi2 ))
-            # estimated r is given by:
-            r_fit = r_v[np.argmin(logL)]
-            if r_fit == 1e-5: r_fit = 0.0
-            # and the 1-sigma error bar by (numerical recipies)
-            ind_sigma = np.argmin(np.abs( (logL[np.argmin(logL):] - logL[np.argmin(logL)]) - 1.00 ))    
-            sigma_r_fit =  r_v[ind_sigma+np.argmin(logL)] - r_fit
-
-            likelihood_on_r_computation( r_fit, make_figure=True )
-
-            print('NB: sigma(r) is ', sigma_r_fit, ' ( +/- ', r_v[ind_sigma+np.argmin(logL)-1] - r_fit, ' , ', r_v[ind_sigma+np.argmin(logL)+1] - r_fit, ' ) ')
-            print('-----')
-
-            return r_fit, sigma_r_fit, likelihood_on_r, chi2
-
 
         print('cosmological analysis now ... ')
         ## data first
@@ -267,15 +206,82 @@ class BBREstimation(PipelineStage):
 
         else:
 
-            r_v = np.logspace(-5,0,num=1000)
+            def from_Cl_to_r_estimate(ClBB_obs, ell_v, fsky, Cl_BB_prim, ClBB_model_other_than_prim, r_v, bins, Cl_BB_lens_bin, **minimize_kwargs):
 
-            r_fit, sigma_r_fit, gridded_likelihood, gridded_chi2 = from_Cl_to_r_estimate(ClBB_obs,
-                                ell_v, self.config['fsky'], Cl_BB_prim_r1,
-                                       ClBB_model_other_than_prim, r_v, bins, Cl_BB_lens_bin)
-            pl.figure()
-            pl.semilogx(r_v, gridded_likelihood)
-            pl.savefig(self.get_output('likelihood_on_r'))
-            # pl.show()
+                def likelihood_on_r_computation( r_loc, make_figure=False ):
+                    '''
+                    -2logL = sum_ell [ (2l+1)fsky * ( log(C) + C^-1.D  ) ]
+                        cf. eg. Tegmark 1998
+                    '''    
+                    Cov_model = bins.bin_cell(Cl_BB_prim[:3*self.config['nside']]*r_loc)[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]\
+                                                + ClBB_model_other_than_prim
+                    if make_figure:
+                        pl.figure( figsize=(10,7), facecolor='w', edgecolor='k' )
+                        ell_v_loc = ell_v[(ell_v>=lmin)&(ell_v<=lmax)]
+                        norm = ell_v_loc*(ell_v_loc+1)/2/np.pi
+                        pl.loglog( ell_v_loc, norm*bins.bin_cell(Cl_BB_prim[:3*self.config['nside']]*r_loc)[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])],
+                                     label='primordial BB, r = '+str(r_loc), linestyle='--', color='Purple', linewidth=2.0 )
+                        pl.loglog( ell_v_loc, norm*Cl_BB_lens_bin[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])], 
+                                    label='lensing BB', linestyle='-', color='DarkOrange', linewidth=2.0)
+                        pl.loglog( ell_v_loc, norm*Cl_cov_clean[1][(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])], 
+                                    label='noise post comp sep', linestyle=':', color='DarkBlue')
+                        
+                        pl.loglog( ell_v_loc, norm*Cl_noise[2][(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])],
+                                                     label='actual dust noise post comp sep', linestyle=':', color='DarkGray')
+                        pl.loglog( ell_v_loc, norm*A_dust*Cl_dust_obs, label='estimated dust template', linestyle='-', color='DarkGray', linewidth=2.0, alpha=0.8)
+                        
+                        pl.loglog( ell_v_loc, norm*ClBB_obs, label='observed BB', color='red', linestyle='-', linewidth=2.0, alpha=0.8)
+                        pl.loglog( ell_v_loc, norm*Cov_model, label='modeled BB', color='k', linestyle='-', linewidth=2.0, alpha=0.8)
+                        pl.legend()
+                        pl.xlabel('$\ell$', fontsize=20)
+                        pl.ylabel('$D_\ell$ $[\mu K^2]$', fontsize=20)
+                        pl.savefig(self.get_output('power_spectrum_post_comp_sep'))
+                        pl.close()
+
+                    # logL = np.sum( (2*ell_v[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]+1)*fsky\
+                                        # *( np.log( Cov_model ) + ClBB_obs/Cov_model ))
+
+                    logL = 0.0 
+                    for b in range(len(ClBB_obs)):
+                        logL += np.sum((2* bins.get_ell_list(b)+1))*fsky*( np.log( Cov_model[b] ) + ClBB_obs[b]/Cov_model[b] )
+                    return logL
+
+                # gridding -2log(L)
+                logL = r_v*0.0
+                for ir in range(len(r_v)):
+                    logL[ir] = likelihood_on_r_computation( r_v[ir] )
+                    ind = ir*100.0/len(r_v)
+                    sys.stdout.write("\r  .......... gridding the likelihood on tensor-to-scalar ratio >>>  %d %% " % ind )
+                    sys.stdout.flush()
+                sys.stdout.write("\n")
+                # renormalizing logL 
+                chi2 = (logL - np.min(logL))
+                # computing the likelihood itself, for plotting purposes
+                likelihood_on_r = np.exp( - chi2 )/np.max(np.exp( - chi2 ))
+                # estimated r is given by:
+                r_fit = r_v[np.argmin(logL)]
+                if r_fit == 1e-5: r_fit = 0.0
+                # and the 1-sigma error bar by (numerical recipies)
+                ind_sigma = np.argmin(np.abs( (logL[np.argmin(logL):] - logL[np.argmin(logL)]) - 1.00 ))    
+                sigma_r_fit =  r_v[ind_sigma+np.argmin(logL)] - r_fit
+
+                likelihood_on_r_computation( r_fit, make_figure=True )
+
+                print('NB: sigma(r) is ', sigma_r_fit, ' ( +/- ', r_v[ind_sigma+np.argmin(logL)-1] - r_fit, ' , ', r_v[ind_sigma+np.argmin(logL)+1] - r_fit, ' ) ')
+                print('-----')
+
+                return r_fit, sigma_r_fit, likelihood_on_r, chi2
+
+
+                r_v = np.logspace(-5,0,num=1000)
+
+                r_fit, sigma_r_fit, gridded_likelihood, gridded_chi2 = from_Cl_to_r_estimate(ClBB_obs,
+                                    ell_v, self.config['fsky'], Cl_BB_prim_r1,
+                                           ClBB_model_other_than_prim, r_v, bins, Cl_BB_lens_bin)
+                pl.figure()
+                pl.semilogx(r_v, gridded_likelihood)
+                pl.savefig(self.get_output('likelihood_on_r'))
+                # pl.show()
 
         print('r_fit = ', r_fit)
         print('sigma_r_fit = ', sigma_r_fit)
