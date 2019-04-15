4c6d326d678cc5ffca6c82c6ea16d2841f30b1df
Josquin Errard <josquin.errard@gmail.com>
2019-04-15 14:08:46 +0200

    debugging

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -189,7 +189,7 @@ class BBREstimation(PipelineStage):
             
             def lnprior( p_loc ): 
                 r_loc, A_dust = p_loc 
-                if -5e-3<=r_loc:#  and -1e-2<=A_dust:
+                if -5e-3<=r_loc  and 0.0<=A_dust:
                     return 0.0
                 return -np.inf
 
