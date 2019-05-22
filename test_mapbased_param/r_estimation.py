b76cdf65e521e5d408bb8fc6ac4882382b2bba33
Josquin Errard <josquin.errard@gmail.com>
2019-05-22 15:10:41 +0200

    setting a lower bound on A_dust

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -207,7 +207,7 @@ class BBREstimation(PipelineStage):
                 labels =  ["r", "\Lambda_d", "\Lambda_s"]
             else:
                 # bounds = [(0.0, None), (0.0, None)]
-                bounds = [(None, None), (None, None)]
+                bounds = [(None, None), (0.0, None)]
                 p0 = [1.0,0.1]
                 names = ["r", "\Lambda_d",]
                 labels =  ["r", "\Lambda_d"]
