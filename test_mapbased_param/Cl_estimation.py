3475d41201e5ae7e314b01190aade6d420af9747
Josquin Errard <josquin.errard@gmail.com>
2019-06-25 14:58:55 +0200

    try with nh calibration

--- test_mapbased_param/Cl_estimation.py
+++ test_mapbased_param/Cl_estimation.py
@@ -83,10 +83,8 @@ class BBClEstimation(PipelineStage):
         mask_apo = nmt.mask_apodization(mask, self.config['aposize'], apotype=self.config['apotype'])
 
         if ((self.config['noise_option']!='white_noise') 
-                and (self.config['noise_option']!='no_noise')
-                    and (not self.config['no_inh'])):
-            ##### mask_nh = mask*np.sqrt(nh)
-            ##### mask_apo *= np.sqrt(nh)
+                and (self.config['noise_option']!='no_noise')):
+                    # and (not self.config['no_inh'])):
             nh = hp.smoothing(nh, fwhm=1*np.pi/180.0, verbose=False) 
             nh /= nh.max()
             mask_apo *= nh
