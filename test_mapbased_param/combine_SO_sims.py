cc4ca37e4ae85d802044839116c3881fac9261a1
Josquin Errard <josquin.errard@gmail.com>
2019-07-04 19:35:15 -0700

    looking for troubles

--- test_mapbased_param/combine_SO_sims.py
+++ test_mapbased_param/combine_SO_sims.py
@@ -14,14 +14,30 @@ CMB_loc = hp.read_map(os.path.join(path_to_files, 'cmb/0010/simonsobs_cmb_uKCMB_
 output_freq_maps = [np.zeros_like(CMB_loc)]*6
 freqs = ['027', '039', '093', '145', '225', '280']
 sims_ = '0010' # because there is a single foregrounds realization
-
+freqs_ = [27, 39, 93, 145, 225, 280]
+std_cmb = []
+std_dust = []
+std_sync = []
+pl.figure()
 indf = 0
 for f in freqs:
 	for s in ['cmb', 'dust', 'synchrotron']:
 		output_freq_maps[indf] += hp.read_map(os.path.join(path_to_files, s+'/'+sims_+'/simonsobs_'+s+'_uKCMB_sa'+f+'_nside512_0010.fits'), field=None)
-
+		if s == 'cmb': std_cmb.append(np.std(output_freq_maps[indf]))
+		if s == 'dust': std_dust.append(np.std(output_freq_maps[indf]))
+		if s == 'sync': std_sync.append(np.std(output_freq_maps[indf]))
 	hp.write_map('./201901_gaussian_fg_lensed_cmb_uKCMB_sa'+f+'_nside512_'+sims_+'.fits', output_freq_maps[indf])
 	
 	indf+= 1
+
+pl.figure()
+pl.plot(freqs_, std_cmb, 'k-', label='CMB')
+pl.plot(freqs_, std_dust, 'r-', label='dust')
+pl.plot(freqs_, std_sync, 'b-', label='sync')
+pl.legend()
+pl.xlabel('frequency [GHz]')
+pl.ylabel('standard deviation [uK_CMB]')
+pl.show()
+
 
 exit()
