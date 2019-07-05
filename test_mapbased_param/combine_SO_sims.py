44ea941d75fc6167cdc6ad718cc468708292bf77
Josquin Errard <josquin.errard@gmail.com>
2019-07-04 19:38:46 -0700

    looking for troubles

--- test_mapbased_param/combine_SO_sims.py
+++ test_mapbased_param/combine_SO_sims.py
@@ -26,7 +26,7 @@ for f in freqs:
 		output_freq_maps[indf] += hp.read_map(os.path.join(path_to_files, s+'/'+sims_+'/simonsobs_'+s+'_uKCMB_sa'+f+'_nside512_0010.fits'), field=None)
 		if s == 'cmb': std_cmb.append(np.std(output_freq_maps[indf][1]))
 		if s == 'dust': std_dust.append(np.std(output_freq_maps[indf][1]))
-		if s == 'sync': std_sync.append(np.std(output_freq_maps[indf][1]))
+		if s == 'synchrotron': std_sync.append(np.std(output_freq_maps[indf][1]))
 	hp.write_map('./201901_gaussian_fg_lensed_cmb_uKCMB_sa'+f+'_nside512_'+sims_+'.fits', output_freq_maps[indf], overwrite=True)
 	
 	indf+= 1
