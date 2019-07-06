4e67f60e70486896fdd91cb4c5683e34dbf20af5
Josquin Errard <josquin.errard@gmail.com>
2019-07-05 17:20:44 -0700

    adding the possibility of having disconnected Bd regions

--- test_mapbased_param/mapbased_parametric_analysis.py
+++ test_mapbased_param/mapbased_parametric_analysis.py
@@ -73,8 +73,10 @@ class BBMapParamCompSep(PipelineStage):
             slices = np.arange(np.min(Bd_template[obs_pix]), np.max(Bd_template[obs_pix]), delta_Bd_patch )
             print('these are the slices : ', slices)
             for i in range(self.config['Nspec']):
-                print('these are the list of indices : ', np.where((Bd_template[obs_pix] >= slices[i] ) & (Bd_template[obs_pix] < slices[i+1]))[0])
-                mask_patches[(Bd_template[obs_pix] >= slices[i] ) & (Bd_template[obs_pix] < slices[i+1])] = 1
+                pix_within_patch = np.where((Bd_template[obs_pix] >= slices[i] ) & (Bd_template[obs_pix] < slices[i+1]))[0]
+                print('these are the list of indices : ', pix_within_patch)
+                print(mask_patches[pix_within_patch])
+                mask_patches[pix_within_patch] = 1
         else:
             mask_patches = [binary_mask]
 
