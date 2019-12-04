5d30ec2eb425b8e677120a9caf8d6bc7acf7d387
Maximilian H Abitbol <mabitbol15@gmail.com>
2019-12-04 12:11:15 +0000

    edgecase

--- bashscripts/oppshifts.sh
+++ bashscripts/oppshifts.sh
@@ -7,22 +7,12 @@ do
     sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[-0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
     sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[0.1\]\]/" $fdir"runconfig.yml"
     /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
-    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_-0.1_0.1.npz"
+    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_-0.10_0.10.npz"
 
     sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
     sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[-0.1\]\]/" $fdir"runconfig.yml"
     /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
-    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.1_-0.1.npz"
-
-    sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
-    sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[0.1\]\]/" $fdir"runconfig.yml"
-    /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
-    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.1_0.1$k.npz"
-
-    sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[-0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
-    sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[-0.1\]\]/" $fdir"runconfig.yml"
-    /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
-    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_-0.1_-0.1$k.npz"
+    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.10_-0.10.npz"
 
     for ((k=1; k<=9; k++))
     do
--- bashscripts/pairshifts.sh
+++ bashscripts/pairshifts.sh
@@ -4,6 +4,17 @@ fdir="./updated_runs/bias_shifts/pairwise/"
 for j in 1 3 5
 do 
     jj=$((j+1))
+
+    sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+    sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[0.1\]\]/" $fdir"runconfig.yml"
+    /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.10.npz"
+
+    sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[-0.1\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+    sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[-0.1\]\]/" $fdir"runconfig.yml"
+    /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+    cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_-0.10.npz"
+
     for ((k=0; k<=9; k++))
     do
         sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[-0.0$k\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
@@ -19,3 +30,5 @@ do
         cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.0$k.npz"
     done
 done
+
+
