29e24741d0fc4fea9622ee8bcc0fc0f566ba7625
Maximilian H Abitbol <mabitbol15@gmail.com>
2019-12-04 11:37:58 +0000

    bias scripts

---
+++ bashscripts/pairgains.sh
@@ -0,0 +1,21 @@
+#~/usr/bin/bash
+
+fdir="./updated_runs/bias_gains/pairwise/"
+for j in 1 3 5
+do 
+    jj=$((j+1))
+    for ((k=0; k<=9; k++))
+    do
+        sed "s/gain\_$j: \['gain', 'fixed', \[1.\]\]/gain\_$j: \['gain', 'fixed', \[0.9$k\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+        sed -i "s/gain\_$jj: \['gain', 'fixed', \[1.\]\]/gain\_$jj: \['gain', 'fixed', \[0.9$k\]\]/" $fdir"runconfig.yml"
+        /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+        cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_gain"$j$jj"_0.9$k.npz"
+    done
+    for ((k=0; k<=9; k++))
+    do
+        sed "s/gain\_$j: \['gain', 'fixed', \[1.\]\]/gain\_$j: \['gain', 'fixed', \[1.0$k\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+        sed -i "s/gain\_$jj: \['gain', 'fixed', \[1.\]\]/gain\_$jj: \['gain', 'fixed', \[1.0$k\]\]/" $fdir"runconfig.yml"
+        /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+        cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_gain"$j$jj"_1.0$k.npz"
+    done
+done
---
+++ bashscripts/pairshifts.sh
@@ -0,0 +1,21 @@
+#~/usr/bin/bash
+
+fdir="./updated_runs/bias_shifts/pairwise/"
+for j in 1 3 5
+do 
+    jj=$((j+1))
+    for ((k=0; k<=9; k++))
+    do
+        sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[-0.0$k\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+        sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[-0.0$k\]\]/" $fdir"runconfig.yml"
+        /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+        cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_-0.0$k.npz"
+    done
+    for ((k=0; k<=9; k++))
+    do
+        sed "s/shift\_$j: \['shift', 'fixed', \[0.\]\]/shift\_$j: \['shift', 'fixed', \[0.0$k\]\]/" $fdir"config.yml" > $fdir"runconfig.yml"
+        sed -i "s/shift\_$jj: \['shift', 'fixed', \[0.\]\]/shift\_$jj: \['shift', 'fixed', \[0.0$k\]\]/" $fdir"runconfig.yml"
+        /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe $fdir"settings.yml"
+        cp $fdir"output/sampler_out.npz" $fdir"autoresults/eb_shift"$j$jj"_0.0$k.npz"
+    done
+done
