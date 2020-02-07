0e034b02ec7bb48653d8a8047717b952464b09c0
Josquin Errard <josquin.errard@gmail.com>
2020-02-07 14:02:05 +0100

    Davids idea for parallelization

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -269,7 +269,7 @@ def main():
         # p.communicate()[0]z
         # p.wait()
         # p = subprocess.check_output("/global/homes/j/josquin/.local/cori/3.6-anaconda-5.2/bin/bbpipe "+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"))
-        p = os.system( args.path_to_bbpipe+' '+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml")
+        p = os.system( args.path_to_bbpipe+' '+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml"))
 
         #         p = os.system( args.path_to_bbpipe+' '+os.path.join(args.path_to_temp_files, "test_"+id_tag+".yml --dry-run > log_"+str(rank)+".txt"))
 
