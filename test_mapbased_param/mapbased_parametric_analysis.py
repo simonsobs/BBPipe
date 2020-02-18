8422775df69bddff718379de377e7299baf9e452
Josquin Errard <josquin.errard@gmail.com>
2020-02-18 16:13:04 +0100

    trying to define clusters differently

--- test_mapbased_param/mapbased_parametric_analysis.py
+++ test_mapbased_param/mapbased_parametric_analysis.py
@@ -80,7 +80,9 @@ class BBMapParamCompSep(PipelineStage):
                 # the number of pixels within each slice will be constant
                 Npix_in_slice = int(len(Bd_template[obs_pix])*1.0/self.config['Nspec'])
                 # generic slices of the template Bd
-                delta_Bd_slices = np.linspace(np.min(Bd_template[obs_pix]), np.max(Bd_template[obs_pix])*(1.0+1.0/len(obs_pix)), num=1000)
+                Nsteps=1000
+                step = (np.max(Bd_template[obs_pix]) - np.min(Bd_template[obs_pix]))/Nsteps
+                delta_Bd_slices = np.linspace(np.min(Bd_template[obs_pix]), np.max(Bd_template[obs_pix])*(1+step), num=Nsteps)
 
                 # loop over the histogram bins
                 ind = 0
