473e2c8a868968f3df21a32ebd177159c12128d6
Josquin Errard <josquin.errard@gmail.com>
2020-02-18 16:24:41 +0100

    trying to define clusters differently

--- test_mapbased_param/mapbased_parametric_analysis.py
+++ test_mapbased_param/mapbased_parametric_analysis.py
@@ -90,7 +90,10 @@ class BBMapParamCompSep(PipelineStage):
                 pix_in_slice = []#np.zeros(self.config['Nspec'])
                 for b in delta_Bd_slices[:-1]:
                     if Npix_in_this_slice < Npix_in_slice: 
-                        pix_in_slice += list(np.where((Bd_template[obs_pix] >= delta_Bd_slices[ind]) & (Bd_template[obs_pix] < delta_Bd_slices[ind+1]) )[0])
+                        if ind == 0:
+                            pix_in_slice += list(np.where((Bd_template[obs_pix] >= delta_Bd_slices[ind]) & (Bd_template[obs_pix] < delta_Bd_slices[ind+1]) )[0])
+                        else:
+                            pix_in_slice[-1] += list(np.where((Bd_template[obs_pix] >= delta_Bd_slices[ind]) & (Bd_template[obs_pix] < delta_Bd_slices[ind+1]) )[0])
                         Npix_in_this_slice += len(pix_in_slice)
                     else:
                         pix_in_slice.append(list(np.where((Bd_template[obs_pix] >= delta_Bd_slices[ind]) & (Bd_template[obs_pix] < delta_Bd_slices[ind+1]) )[0]))
