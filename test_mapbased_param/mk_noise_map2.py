e687e870236c8c17bb88606f849f16bc8d9c1035
Josquin Errard <josquin.errard@gmail.com>
2020-03-27 15:02:09 +0100

    new noise generator with CMBS4 specifications

--- test_mapbased_param/mk_noise_map2.py
+++ test_mapbased_param/mk_noise_map2.py
@@ -73,13 +73,14 @@ def get_noise_sim(sensitivity=2,knee_mode=1,ny_lf=1.,nside_out=512, \
 
     for i_n in np.arange(len(nll)) :
         n=nll[i_n]
-        nl=np.zeros(3*nside_out)
         if CMBS4_opt:
+            nl=np.zeros((3, 3*nside_out))
             for i in range(3): 
-                nl[i][2:]=n; nl[i][:2]=n[i][0]
+                nl[i,2:]=n; nl[i,:2]=n[i][0]
             no_t,no_q,no_u=hp.synfast([nl[0],nl[1],nl[2],0*nl,0*nl,0*nl],nside=nside_out,
                                   pol=True,new=True,verbose=False)
         else:
+            nl=np.zeros(3*nside_out)
             nl[2:]=n; nl[:2]=n[0]
             no_t,no_q,no_u=hp.synfast([nl/2.,nl,nl,0*nl,0*nl,0*nl],nside=nside_out,
                                   pol=True,new=True,verbose=False)
