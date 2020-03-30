1f6c3d8d15c1bfa7fc95d9b2322f499ed6754835
susannaaz <susanna.azzoni@gmail.com>
2020-03-30 15:10:29 +0100

    modified pysm beta

--- examples/new_sim.py
+++ examples/new_sim.py
@@ -29,11 +29,11 @@ parser.add_option('--do-cl', dest='do_Cls', default=True,  action='store_true',
 parser.add_option('--beta-var', dest='beta_var', default=False,  action='store_true',
                   help='Set to include gaussian spectral indices, default=False')
 parser.add_option('--beta-pysm', dest='beta_pysm', default=False,  action='store_true',
-                  help='Set to include gaussian spectral indices, default=False')
+                  help='Set to include non-gaussian varying spectral indices, default=False')
 parser.add_option('--sigma-d', dest='sigma_dust', default=10,  type=int,
-                  help='Modify amplitude of dust variation, default=10*E-2')
+                  help='Modify amplitude of dust variation, default=0. Input values are multiplied to E-2')
 parser.add_option('--sigma-s', dest='sigma_sync', default=0,  type=int,
-                  help='Modify amplitude of sync variation, default=10*E-2')
+                  help='Modify amplitude of sync variation, default=0. Input values are multiplied to E-2')
 
 (o, args) = parser.parse_args()
 
@@ -43,7 +43,7 @@ np.random.seed(seed)
 
 if len(args) != 9:
         #parser.error
-        print("Default settings: --seed --nside --simulate --pysm --do-cl --beta-var --sigma-d --sigma-s")
+        print("Default settings: --seed --nside --simulate --pysm --do-cl --beta-var --beta-pysm --sigma-d --sigma-s")
         print("Check with <script> -h")
 
 prefix_in='/mnt/zfsusers/susanna/PySM-tests2/BBPipe/examples/'
@@ -57,9 +57,8 @@ alpha_dust_BB=-0.2
 nu0_dust=353. 
 temp_dust = 19.6
 if o.beta_pysm:
-        beta_dust = hp.ud_grade(hp.read_map(prefix_in+'template_PySM/beta_mean1p59_std0p2.fits', verbose=False), nside_out=nside)
-if o.beta_var:
-        #print('map_beta_dust_Ad%d.fits'%(A_bd))
+        beta_dust = hp.ud_grade(hp.read_map(prefix_in+'template_PySM/dust_beta.fits', field=0, verbose=False), nside_out=nside)
+elif o.beta_var:
         beta_dust =  hp.ud_grade(hp.read_map(prefix_in+'map_beta_dust_sigD%d_sd%d.fits'%(o.sigma_dust, seed), verbose=False), nside_out=nside)
 else:
     beta_dust = 1.59
@@ -109,8 +108,13 @@ if o.use_pysm:
         dirname = prefix_out+"new_sim_ns%d_seed%d_pysm_betaVar_sigD%dsigS%d"%(nside, seed, o.sigma_dust, o.sigma_sync)
         for i in beta_dust:
                 f_dust = dust_sed(nu, nu0_dust, i, temp_dust)
+        f_sync = power_law(nu, nu0_sync, beta_sync)
         #for j in beta_sync:
         #        f_sync = power_law(nu, nu0_sync, j)
+    elif o.beta_pysm:
+        dirname = prefix_out+"new_sim_ns%d_seed%d_pysm_betapysm"%(nside, seed)
+        for i in beta_dust:
+                f_dust = dust_sed(nu, nu0_dust, i, temp_dust)
         f_sync = power_law(nu, nu0_sync, beta_sync)
     else:
         dirname = prefix_out+"new_sim_ns%d_seed%d_pysm"%(nside, seed)
@@ -136,6 +140,11 @@ else:
             f_dust = comp_sed(nu, nu0_dust, i, temp_dust, 'dust')
         #for j in beta_sync:
         #        f_sync = comp_sed(nu, nu0_sync, j, None, 'sync')
+        f_sync = comp_sed(nu, nu0_sync, beta_sync, None, 'sync')
+    elif o.beta_pysm:
+        dirname = prefix_out+"new_sim_ns%d_seed%d_bbsim_betapysm"%(nside, seed)
+        for i in beta_dust:
+            f_dust = comp_sed(nu, nu0_dust, i, temp_dust, 'dust')
         f_sync = comp_sed(nu, nu0_sync, beta_sync, None, 'sync')
     else:
         dirname = prefix_out+"new_sim_ns%d_seed%d_bbsim"%(nside, seed)
--- examples/run_many_newsims.sh
+++ examples/run_many_newsims.sh
@@ -21,29 +21,29 @@ python_exec="/usr/bin/python3"
 #done
 
 
-nside=256
-sig_s=0
-for seed in 1000 1001 1002 1003 1004
-do
-    for sig_d in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 23 25 30 40 50 60 70 80 90 100
-    do
-	if [ ! -f map_beta_dust_sigD${sig_d}_sd${seed}.fits ] || [ ! -f map_beta_sync_sigS${sig_s}.fits ] ; then
-	    echo "Missing spectral index maps with sigma = E-2*"$sig_d
-	    echo "Generating gaussian dust spectral indices with sigma "
-	    python3 beta_gaussian.py --sigma-d ${sig_d} --seed ${seed}
-	fi
-	if [ ! -d new_sim_ns${nside}_seed${seed}_bbsim_betaVar_sigD${sig_d}sigS${sig_s} ] ; then
-	    echo "Generating simulation: gaussian beta with sigma dust = "${sig_d}
-	    ${python_exec} new_sim.py --seed ${seed} --beta-var True --sigma-d ${sig_d}
-	    echo "/mnt/zfsusers/susanna/PySM-tests2/BBPipe/examples/new_sim_ns${nside}_seed${seed}_bbsim_betaVar_sigD${sig_d}sigS${sig_s}" >>  list_newsims_seed1300_sigDsigS.txt
-	fi
-    done
-done
-#  
+#nside=256
+#sig_s=0
+#for seed in 1000 1001 1002 1003 1004
+#do
+#    for sig_d in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 23 25 30 40 50 60 70 80 90 100
+#    do
+#	if [ ! -f map_beta_dust_sigD${sig_d}_sd${seed}.fits ] || [ ! -f map_beta_sync_sigS${sig_s}.fits ] ; then
+#	    echo "Missing spectral index maps with sigma = E-2*"$sig_d
+#	    echo "Generating gaussian dust spectral indices with sigma "
+#	    python3 beta_gaussian.py --sigma-d ${sig_d} --seed ${seed}
+#	fi
+#	if [ ! -d new_sim_ns${nside}_seed${seed}_bbsim_betaVar_sigD${sig_d}sigS${sig_s} ] ; then
+#	    echo "Generating simulation: gaussian beta with sigma dust = "${sig_d}
+#	    ${python_exec} new_sim.py --seed ${seed} --beta-var True --sigma-d ${sig_d}
+#	    echo "/mnt/zfsusers/susanna/PySM-tests2/BBPipe/examples/new_sim_ns${nside}_seed${seed}_bbsim_betaVar_sigD${sig_d}sigS${sig_s}" >>  list_newsims_seed1300_sigDsigS.txt
+#	fi
+#    done
+#done
+##  
 
 # Generate simulations with pysm beta maps
-#for seed in 1300 1301 1302 1303 1304
-#do
-#    echo "Generating simulation: pysm beta"
-#    ${python_exec} new_sim.py --seed ${seed} --beta-pysm True 
-#done
+for seed in 1300 1301 1302 1303 1304
+do
+    echo "Generating simulation: pysm beta"
+    ${python_exec} new_sim.py --seed ${seed} --beta-pysm True 
+done
