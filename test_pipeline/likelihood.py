71a5785d92d85659b12b706143f785015e9f9659
David Alonso <dam.phys@gmail.com>
2020-12-10 15:45:10 +0000

    unit test

--- test_pipeline/likelihood.py
+++ test_pipeline/likelihood.py
@@ -15,9 +15,10 @@ class Likelihood(PipelineStage):
         self.ls = d['ls']
         self.cl = d['cl']
         self.tl = cl_in[self.ls]
-        self.icov = d['cov']
+        self.icov = np.linalg.inv(d['cov'])
 
     def run(self):
+        self.set_up()
         s2_a = 1./np.dot(self.tl, np.dot(self.icov, self.tl))
         a = s2_a * np.dot(self.tl, np.dot(self.icov, self.cl))
         np.savez(self.get_output('results'), a=a, sigma_a=np.sqrt(s2_a))
