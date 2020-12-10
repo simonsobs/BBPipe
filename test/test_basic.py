3ca0b0a42d71a15bf8c34ed3757bb37100ab06f7
David Alonso <dam.phys@gmail.com>
2020-12-10 16:09:08 +0000

    path

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,6 +2,6 @@ import numpy as np
 
 
 def test_pipeline_output():
-    d = np.load('test/results.npz')
+    d = np.load('test/Likelihood/results.npz')
     # Less than a 5-sigma deviation
     assert np.fabs(d['a']-1)/d['sigma_a'] < 5
