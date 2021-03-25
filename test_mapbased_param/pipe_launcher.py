fe5efdf76fa69f435b0e1c443a1f64544e044a5b
Josquin Errard <josquin.errard@gmail.com>
2021-03-25 14:31:03 +0100

    getting ready for analysis of map-based simulations

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -318,7 +318,7 @@ def main():
             for f in frequencies:
 
                 if not os.path.isfile(os.path.join(args.combined_directory, str(i_sim).zfill(4)+'/SO_SAT_'+str(f)+'_comb_'+str(i_sim).zfill(4)+'.fits')):
-                    
+
                     if os.path.isfile(os.path.join(args.combined_directory, str(i_sim).zfill(4)+'/SO_SAT_'+str(f)+'_comb_'+str(i_sim).zfill(4)+'.fits')): continue
                     dust = hp.read_map( glob.glob(os.path.join(args.external_sky_sims, 'FG_20201207/gaussian/foregrounds/dust/'+str(i_sim).zfill(4)+'/SO_SAT_'+str(f)+'_dust_'+str(i_sim).zfill(4)+'*.fits'))[0], field=None)
                     synch = hp.read_map( glob.glob(os.path.join(args.external_sky_sims, 'FG_20201207/gaussian/foregrounds/synch/'+str(i_sim).zfill(4)+'/SO_SAT_'+str(f)+'_synch_'+str(i_sim).zfill(4)+'*.fits'))[0], field=None)
@@ -390,7 +390,11 @@ def main():
         fin.close()
         fout.close()
 
-        p = os.system('sbatch batch_'+id_tag+".sh")
+        # submit the job if the final products have not be produced already
+        if os.path.isfile(os.path.join(args.path_to_temp_files,'outputs_'+id_tag,'estimated_cosmo_params.txt'))
+            print('this has already been computed! '+os.path.join(args.path_to_temp_files,'outputs_'+id_tag,'estimated_cosmo_params.txt'))
+        else:
+            p = os.system('sbatch batch_'+id_tag+".sh")
 
     ####################
     if mpi: barrier()
