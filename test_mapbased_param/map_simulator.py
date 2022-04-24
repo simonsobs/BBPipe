noise covariance fix

test_mapbased_param/map_simulator.py
@@ -480,12 +480,13 @@ class BBMapSim(PipelineStage):
             print('/// BYPASS NOISE COV')
             tag = ''
             for f in instrument.frequency: tag += str(f)+'_'
-            for key in ['common_beam_correction', 'no_inh', 'Nico_noise_combination', 'Nsims_bias', 'nside']:
+            for key in ['common_beam_correction', 'no_inh', 'Nico_noise_combination', 'Nsims_bias', 'nside', 'sensitivity_mode', 'knee_mode']:
                 tag += key+'_'+self.config[key]
             path_to_noise_cov = os.path.join('/global/cscratch/sd/josquin/bypass_noise_cov_'+tag)
             if not os.path.exists(path_to_noise_cov+'.npy'):
+                print('noise covariance is not on disk yet. Computing it now.')
                 noise_cov, noise_cov_beamed = noise_covariance_estimation(self, freq_maps.shape, instrument, nhits)
-                np.save(path_to_noise_cov, (noise_cov, noise_cov_beamed) )
+                np.save(path_to_noise_cov, (noise_cov, noise_cov_beamed), allow_pickle=True)
             else: 
                 noise_cov, noise_cov_beamed = np.load(path_to_noise_cov+'.npy')
         else: