0b147212f8dfa2ec803b542274868394a34e815d
josquin-e <josquin.errard@gmail.com>
2022-06-10 16:27:27 +0200

    handling non symmetric distribution of recovered r

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -550,16 +550,19 @@ def main():
         # find the positive error bar
         rs_pos = bins_m[bins_m > r_fit]
         plike_pos = n_r[bins_m > r_fit]
+        print('n_r[bins_m > r_fit] = ', n_r[bins_m > r_fit])
         cum_pos = np.cumsum(plike_pos)
         cum_pos /= cum_pos[-1]
         sigma_r_pos = rs_pos[np.argmin(np.abs(cum_pos -  0.68))] - r_fit
+        print('rs_pos[np.argmin(np.abs(cum_pos -  0.68))] = ', rs_pos[np.argmin(np.abs(cum_pos -  0.68))])
         # find the positive error bar
         rs_neg = bins_m[bins_m < r_fit]
         plike_neg = n_r[bins_m < r_fit]
         cum_neg = np.cumsum(plike_neg[::-1])
         cum_neg /= cum_neg[-1]
         sigma_r_neg = r_fit - rs_neg[::-1][np.argmin(np.abs(cum_neg -  0.68))]
-        ax[0].set_title('r = '+str(round(r_fit,5))+' + '+str(round(sigma_r_pos,5))+' - '+str(round(sigma_r_neg,5)), fontsize=10)
+        print('r = '+str(r_fit)+' + '+str(sigma_r_pos)+' - '+str(sigma_r_neg)
+        ax[0].set_title('r = '+str(round(r_fit,5))+' + '+str(round(sigma_r_pos,7))+' - '+str(round(sigma_r_neg,7)), fontsize=10)
 
         # ax[0].axvline(x=np.mean(r_all), color='DarkGray', linestyle='--', alpha=0.8, linewidth=2.0)
         # ax[0,1].set_title('sigma(r), '+str(np.mean(sigma_all))+' +/- '+str(np.std(sigma_all)))
