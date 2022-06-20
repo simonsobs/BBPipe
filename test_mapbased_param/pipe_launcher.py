449a270fa441cbee034d916b85183c2168fc083a
josquin-e <josquin.errard@gmail.com>
2022-06-20 14:23:12 +0200

    ability to fit by a Gaussian the histogram of r and Al

--- test_mapbased_param/pipe_launcher.py
+++ test_mapbased_param/pipe_launcher.py
@@ -574,7 +574,7 @@ def main():
         # find the max 
         from scipy.stats import norm
         (mu_r, sigma_r) = norm.fit(r_all)
-        y = norm.normpdf( bins, mu, sigma)
+        y = norm.pdf( bins, mu, sigma)
         ax[0].plot(bins, y, 'r--', linewidth=2)
         r_fit = mu_r#bins_m[np.argmax(n_r)]
 
@@ -631,7 +631,7 @@ def main():
             # find the max 
             print('n_AL = ', n_AL)
             (mu_AL, sigma_AL) = norm.fit(AL_all)
-            y_AL = norm.normpdf( bins_AL, mu_AL, sigma_AL)
+            y_AL = norm.pdf( bins_AL, mu_AL, sigma_AL)
             ax[0].plot(bins_AL, y_AL, 'r--', linewidth=2)
             AL_fit = mu_AL#bins_AL_m[np.argmax(n_AL[1:-1])+1]
             print('AL_fit=', AL_fit)
