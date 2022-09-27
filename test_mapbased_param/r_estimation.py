a38e81afaa239b177ef7159f6f229ee4ee77b5df
josquin-e <josquin.errard@gmail.com>
2022-09-27 09:18:06 +0200

    fixing typo

--- test_mapbased_param/r_estimation.py
+++ test_mapbased_param/r_estimation.py
@@ -182,7 +182,7 @@ class BBREstimation(PipelineStage):
                 # Cov_model = bins.bin_cell(Cl_BB_prim_r1[:3*self.config['nside']]*r_loc)[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]\
                                             # + ClBB_model_other_than_prim_and_lens + A_dust*Cl_dust_obs + AL*Cl_BB_lens_bin[(ell_v>=self.config['lmin'])&(ell_v<=self.config['lmax'])]
 
-                Cl_BB_lens_bin_ = bins.bin_cell(AL_loc*Cl_BB_lens[:3*self.config['nside']])
+                Cl_BB_lens_bin_ = bins.bin_cell(AL*Cl_BB_lens[:3*self.config['nside']])
                 ClBB_model_other_than_prim_ = Cl_BB_lens_bin_[(ell_v>=lmin)&(ell_v<=lmax)]
                 if self.config['noise_option']!='no_noise': 
                     ClBB_model_other_than_prim_ += Cl_noise_bias[1][(ell_v>=lmin)&(ell_v<=lmax)]
